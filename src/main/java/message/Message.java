b6a1b3b4f0fb9799c39395e81350c19109f8f3ce
Nandan T S <nanven9@gmail.com>
2026-02-20 21:35:52 +0530

    Migrated constructor, getter and setters to Lombok

--- src/main/java/message/Message.java
+++ src/main/java/message/Message.java
@@ -1,46 +1,16 @@
 package message;
 
-//import lombok.AllArgsConstructor;
-//import lombok.Data;
-//import lombok.Getter;
-//import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-//@Getter
-//@Setter
-//@AllArgsConstructor
+@Getter
+@Setter
+@AllArgsConstructor
 public class Message {
     private int messageSize;
-
-    public MessageHeader getMessageHeader() {
-        return messageHeader;
-    }
-
-    public void setMessageHeader(MessageHeader messageHeader) {
-        this.messageHeader = messageHeader;
-    }
-
-    public int getMessageSize() {
-        return messageSize;
-    }
-
-    public void setMessageSize(int messageSize) {
-        this.messageSize = messageSize;
-    }
-
-    public byte[] getMessageBody() {
-        return messageBody;
-    }
-
-    public void setMessageBody(byte[] messageBody) {
-        this.messageBody = messageBody;
-    }
-
     private MessageHeader messageHeader;
     private byte[] messageBody;
 
-    public Message(int messageSize, MessageHeader messageHeader, byte[] messageBody) {
-        this.messageSize = messageSize;
-        this.messageHeader = messageHeader;
-        this.messageBody = messageBody;
-    }
 }
--- src/main/java/message/MessageHeader.java
+++ src/main/java/message/MessageHeader.java
@@ -1,25 +1,13 @@
 package message;
 
-//import lombok.AllArgsConstructor;
-//import lombok.Data;
-//import lombok.Getter;
-//import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-//@Setter
-//@Getter
-//@AllArgsConstructor
+@Setter
+@Getter
+@AllArgsConstructor
 public class MessageHeader {
-    public int getCorrelationId() {
-        return correlationId;
-    }
-
-    public void setCorrelationId(int correlationId) {
-        this.correlationId = correlationId;
-    }
-
-    int correlationId;
-
-    public MessageHeader(int correlationId) {
-        this.correlationId=correlationId;
-    }
+    private int correlationId;
 }
