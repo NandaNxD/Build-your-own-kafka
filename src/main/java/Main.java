c76fec5d186624b986e6c69ae645533e54503a2f
NANDATS <nandats@tbdir.net>
2026-02-25 15:02:48 +0530

    Added an incomplete encode function on ApiVersionsRequestBody

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -19,10 +19,9 @@ public class Main {
      int port = 9092;
      try {
        serverSocket = new ServerSocket(port);
-       // Since the tester restarts your program quite often, setting SO_REUSEADDR
-       // ensures that we don't run into 'Address already in use' errors
+
        serverSocket.setReuseAddress(true);
-       // Wait for connection from client.
+
        clientSocket = serverSocket.accept();
 
        TCPStreamReader tcpStreamReader=new TCPStreamReader();
